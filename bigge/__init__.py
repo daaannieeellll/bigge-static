01e2e2fd056dc790aa0896c0b23c2e172a25b780
D. Kuiper <d.kuiper.2@umail.leidenuniv.nl>
2021-02-15 21:42:03 +0100

    Ensure HTTPS

--- bigge/__init__.py
+++ bigge/__init__.py
@@ -1,11 +1,22 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+
+    @app.before_request
+    def before_request():
+        if app.env == "development":
+            return
+        if request.is_secure:
+            return
+
+        url = request.url.replace("http://", "https://", 1)
+        code = 301
+        return redirect(url, code=code)
 
     @app.route("/")
     def index():
--- bigge/pwa.py
+++ bigge/pwa.py
@@ -2,7 +2,7 @@ from flask import Blueprint, make_response, send_from_directory
 
 bp = Blueprint('pwa', __name__, url_prefix='')
 
-@bp.route('/manifest.json')
+@bp.route('manifest.json')
 def manifest():
     return send_from_directory('static', 'manifest.json')
 
